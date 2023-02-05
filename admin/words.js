3dcdfc50a24c01e1224a7f86073ec57c89f253df
ioBroker Adapter Creator <noreply@iobroker.net>
2023-02-05 13:09:23 +0000

    Initial commit by adapter creator

---
+++ admin/words.js
@@ -0,0 +1,43 @@
+/* eslint no-unused-vars: off */
+/* eslint no-global-assign: off */
+/* global systemDictionary */
+'use strict';
+
+systemDictionary = {
+	'opendtu adapter settings': {
+		'en': 'Adapter settings for opendtu',
+		'de': 'Adaptereinstellungen für opendtu',
+		'ru': 'Настройки адаптера для opendtu',
+		'pt': 'Configurações do adaptador para opendtu',
+		'nl': 'Adapterinstellingen voor opendtu',
+		'fr': "Paramètres d'adaptateur pour opendtu",
+		'it': "Impostazioni dell'adattatore per opendtu",
+		'es': 'Ajustes del adaptador para opendtu',
+		'pl': 'Ustawienia adaptera dla opendtu',
+		'zh-cn': 'opendtu的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'option1',
+		'ru': 'option1',
+		'pt': 'option1',
+		'nl': 'option1',
+		'fr': 'option1',
+		'it': 'option1',
+		'es': 'option1',
+		'pl': 'option1',
+		'zh-cn': 'option1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'option2',
+		'ru': 'option2',
+		'pt': 'option2',
+		'nl': 'option2',
+		'fr': 'option2',
+		'it': 'option2',
+		'es': 'option2',
+		'pl': 'option2',
+		'zh-cn': 'option2'
+	}
+};
